b1b309f2423e1850c97dd0f78d0fdc6fa81e256a
Ignacio Ortiz <iortizalonzua@gmail.com>
2023-06-19 18:07:32 +0200

    Agregar archivos de proyecto.

---
+++ DataBinding1/DataBinding1/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace DataBinding1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var person = new Person
+            {
+                Name = "Nacho"
+            };
+
+
+            this.BindingContext = person;
+        }
+    }
+}
